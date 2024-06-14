799b9bf76933f00705342f18aa74957cd13256fa
Vishvaparathy <barathisaravanapavan98@gmail.com>
2024-06-14 09:32:54 +0530

    Add files via upload

---
+++ Task03/script.js
@@ -0,0 +1,20 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.getAttribute('data-value');
+
+        if (value === 'C') {
+            display.innerText = '';
+        } else if (value === '=') {
+            try {
+                display.innerText = eval(display.innerText);
+            } catch {
+                display.innerText = 'Error';
+            }
+        } else {
+            display.innerText += value;
+        }
+    });
+});
